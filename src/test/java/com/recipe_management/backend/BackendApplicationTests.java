91fe81153727003cb75d0d88a76a126c73d72fc0
MS <mswan@cmg.com.my>
2025-06-07 18:09:07 +0800

    Initial Commit

---
+++ src/test/java/com/recipe_management/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipe_management.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
