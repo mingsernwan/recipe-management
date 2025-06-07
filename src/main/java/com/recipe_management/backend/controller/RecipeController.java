c9227df84130e2674c3e2bd9aa62b495a914a640
MS <mswan@cmg.com.my>
2025-06-07 19:15:17 +0800

    Completed: Creating Recipe Repository, Service, and IService Files

--- src/main/java/com/recipe_management/backend/controller/RecipeController.java
+++ src/main/java/com/recipe_management/backend/controller/RecipeController.java
@@ -1,12 +1,21 @@
 package com.recipe_management.backend.controller;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.recipe_management.backend.model.Recipe;
+import com.recipe_management.backend.service.IRecipeService;
 import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1/recipes")
 public class RecipeController {
-  
+  private final IRecipeService recipesService;
+
+  @GetMapping
+  public List<Recipe> getRecipeList() {
+    return recipesService.getRecipeList();
+  }
 }
