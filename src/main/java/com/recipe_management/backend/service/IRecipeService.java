692ed17ca4a6e19655bfbc467971687f05487455
wms <58557598+mingsernwan@users.noreply.github.com>
2025-06-07 19:45:09 +0800

    Completed: Creating Save Recipe POST API (#4)
    * Completed: Creating Save Recipe POST API
    
    * Removed Extra Space
    
    ---------
    
    Co-authored-by: MS <mswan@cmg.com.my>

--- src/main/java/com/recipe_management/backend/service/IRecipeService.java
+++ src/main/java/com/recipe_management/backend/service/IRecipeService.java
@@ -1,8 +1,10 @@
 package com.recipe_management.backend.service;
 
 import java.util.List;
+import com.recipe_management.backend.dto.SaveRecipeDTO;
 import com.recipe_management.backend.model.Recipe;
 
 public interface IRecipeService {
   List<Recipe> getRecipeList();
+  void saveRecipe(SaveRecipeDTO saveRecipeDTO);
 }
--- src/main/java/com/recipe_management/backend/service/RecipeService.java
+++ src/main/java/com/recipe_management/backend/service/RecipeService.java
@@ -2,20 +2,33 @@ package com.recipe_management.backend.service;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
+import com.recipe_management.backend.dto.SaveRecipeDTO;
 import com.recipe_management.backend.model.Recipe;
 import com.recipe_management.backend.repository.RecipeRepository;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 
 public class RecipeService implements IRecipeService {
-  private final RecipeRepository recipesRepository;
+  private final RecipeRepository recipeRepository;
 
   @Override
   public List<Recipe> getRecipeList() {
-    List<Recipe> recipeList = recipesRepository.findAll();
+    List<Recipe> recipeList = recipeRepository.findAll();
     return recipeList;
   }
-  
+
+  @Override
+  @Transactional
+  public void saveRecipe(SaveRecipeDTO saveRecipeDTO) {
+    Recipe recipe = new Recipe();
+    recipe.setRecipeName(saveRecipeDTO.recipeName());
+    recipe.setRecipeDescription(saveRecipeDTO.recipeDescription());
+    recipe.setRecipeCookTime(saveRecipeDTO.recipeCookTime());
+    recipe.setRecipeDifficulty(saveRecipeDTO.recipeDifficulty());
+    recipe.setActiveFlag(true );
+    recipeRepository.save(recipe);
+  }
 }
