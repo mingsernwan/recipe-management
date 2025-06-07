644623edbfb40196f0b7ea6acbf74927f2a29070
MS <mswan@cmg.com.my>
2025-06-07 18:27:48 +0800

    Completed: Creating Recipes Model

---
+++ src/main/java/com/recipe_management/backend/model/Recipes.java
@@ -0,0 +1,28 @@
+package com.recipe_management.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "rm_recipes")
+
+
+public class Recipes {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long recipeId;
+    private String recipeName;
+    private String recipeDescription;
+    private String recipeCookTime;
+    private String recipeDifficulty;
+    private Boolean activeFlag;
+}
