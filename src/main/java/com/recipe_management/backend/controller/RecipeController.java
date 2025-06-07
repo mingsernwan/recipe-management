1bf6e20148c0d731ea9d59e6b4e8bf7fe16e6bc1
MS <mswan@cmg.com.my>
2025-06-07 18:17:27 +0800

    Created Recipe Controller File

---
+++ src/main/java/com/recipe_management/backend/controller/RecipeController.java
@@ -0,0 +1,12 @@
+package com.recipe_management.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/recipes")
+public class RecipeController {
+  
+}
